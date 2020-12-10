69f45a687c0a296db440e09fe0801d76a9d9362b
tholliver <michaelmmv@outlook.com>
2020-12-10 15:39:16 -0400

    able to add on pedido with no registered uses [addsU/insertsPedido]

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var http = require("http");
+const https = require("https");
 var express = require("express");
 var app = express();
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
+const axios = require("axios");
+const fetch = require("node-fetch");
 
 var db_config = {
   host: "bpxswzqwuifl3kr6kmvs-mysql.services.clever-cloud.com",
@@ -41,7 +44,7 @@ handleDisconnect();
 app.use(function (req, res, next) {
   res.header("");
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'POST');
+  res.header("Access-Control-Allow-Methods", "POST");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -57,7 +60,6 @@ app.use(
   })
 );
 //end body-parser configuration
-
 
 //create app server
 var port = process.env.PORT || 8000;
@@ -82,88 +84,123 @@ app.get("/pedido", function (req, res) {
 });
 
 app.get("/productopedido/:id", function (req, res) {
-  connection.query("select * from pedidoProductos where pedido_idpedido=?",[req.params.id], function (error, results, fields) {
-    if (error) throw error;
-    res.end(JSON.stringify(results));
-  });
+  connection.query(
+    "select * from pedidoProductos where pedido_idpedido=?",
+    [req.params.id],
+    function (error, results, fields) {
+      if (error) throw error;
+      res.end(JSON.stringify(results));
+    }
+  );
 });
-
 
 //consulta agregada 1
 app.get("/pedidousuario", function (req, res) {
-  connection.query("SELECT idcliente, nombre as nombreCli ,idpedido, direccion, fechaPedido, cantidadTotal, totalPagar FROM pedido, cliente  where pedido.idclienteP = cliente.idcliente", function (error, results, fields) {
-    if (error) throw error;
-    res.end(JSON.stringify(results));
-  });
+  connection.query(
+    "SELECT idcliente, nombre as nombreCli ,idpedido, direccion, fechaPedido, cantidadTotal, totalPagar FROM pedido, cliente  where pedido.idclienteP = cliente.idcliente",
+    function (error, results, fields) {
+      if (error) throw error;
+      res.end(JSON.stringify(results));
+    }
+  );
 });
-
 
 //consulta agregada 2
 app.get("/productopedido", function (req, res) {
-  connection.query("SELECT pedido_idpedido, nombre , cantidadComp, precio FROM pedidoProductos, producto where pedidoProductos.producto_idproducto = producto.idproducto", function (error, results, fields) {
-    if (error) throw error;
-    res.end(JSON.stringify(results));
-  });
+  connection.query(
+    "SELECT pedido_idpedido, nombre , cantidadComp, precio FROM pedidoProductos, producto where pedidoProductos.producto_idproducto = producto.idproducto",
+    function (error, results, fields) {
+      if (error) throw error;
+      res.end(JSON.stringify(results));
+    }
+  );
 });
 
-async function firstFunction(values){
-  console.log('En la funcion 1',values)
-    var someMessage;
-  connection.query("INSERT INTO cliente(idcliente, nombre, username, password) VALUES ?", [values], function(err, result) {
+async function firstFunction(values) {
+  console.log("En la funcion 1", values);
+  var someMessage;
+  connection.query("INSERT INTO cliente SET ?", values, function (err, result) {
     if (err) throw err;
     console.log("Inserted users: " + result.affectedRows);
     someMessage = JSON.stringify(result);
   });
   return someMessage;
-};
+}
 
-async function secondFunction(data){
+async function secondFunction(data) {
   await firstFunction(data);
   // now wait for firstFunction to finish...
   // do something else
-};
+}
+
+//---------------------------------------------
+
+//---------------------------------------------
 
 //New user
-//Firts check if user exits 
-//then if dont 
-app.post("/pedidito", function (req, res){
+//Firts check if user exits
+//then if dont
+app.post("/pedidito", function (req, res) {
   var dataFilds = req.body;
-  var carnet= req.body.idclienteP;
-  let maxxx=[{"idcliente":carnet}];
-  
-  console.log('The pedido data',dataFilds);
-  console.log('Aqui el carnet',carnet);
-  connection.query("select idcliente as masa from cliente where idcliente=?;", maxxx,function (
-    err,
-    result,
-    fields
-  ){
-    if (err) throw err;
-    //console.log(result[0].masa,"");
-    
-    if (result.length > 0) {
-      console.log("User Found");
-      
-      var sql = "INSERT INTO pedido(idpedido, direccion, fechaPedido, cantidadTotal, totalPagar, idclienteP) VALUES ?";
-      
-      
-      connection.query(sql, [dataFilds], function(err, result) {
-        if (err) throw err;
-        console.log(" Number of records inserted on pedidosProductos: " + result.affectedRows);
-        res.end(JSON.stringify(result));
-      });
-    } else {
-      console.log("User not Found");
-      secondFunction(dataFilds);
-      var sql = "INSERT INTO pedido(idpedido, direccion, fechaPedido, cantidadTotal, totalPagar, idclienteP) VALUES ?";
-      
-      connection.query(sql, [dataFilds], function(err, result) {
-        if (err) throw err;
-        console.log(" Number of records inserted on pedidosProductos: " + result.affectedRows);
-        res.end(JSON.stringify(result));
-      });
+  var carnet = req.body.idclienteP.ci;
+  var direccion = req.body.direccion;
+  var fechaPedido=req.body.fechaPedido;
+  var totalPagar = req.body.totalPagar;
+  var cantidadTotal = req.body.cantidadTotal;
+
+  var nameFull = req.body.idclienteP.nombreFull;
+
+  let maxxx = [{ idcliente: carnet }];
+  const newPedido={
+    "direccion":direccion,
+    "fechaPedido":fechaPedido,
+    "cantidadTotal":cantidadTotal,
+    "totalPagar":totalPagar,
+    "idclienteP":carnet
+    };
+  connection.query(
+    "select idcliente as masa from cliente where idcliente=?;",
+    carnet,
+    function (err, result, fields) {
+      if (err) throw err;
+      //console.log(result[0].masa,"");
+
+      if (result.length > 0) {
+        console.log("User Found");
+
+        var sql = "INSERT INTO pedido SET ?";
+
+        connection.query(sql, [newPedido], function (err, result) {
+          if (err) throw err;
+          console.log(
+            " Number of records inserted on pedidos: " +
+              result.affectedRows
+          );
+          res.end(JSON.stringify(result));
+        });
+      } else {
+        console.log("User not Found");
+        console.log(carnet, nameFull);
+        var newUser = {
+          idcliente: carnet,
+          nombre: nameFull,
+          username: "noExiste",
+          password: "noExiste",
+        };
+        secondFunction(newUser);
+        var sql = "INSERT INTO pedido SET ?";
+
+        connection.query(sql, [newPedido], function (err, result) {
+          if (err) throw err;
+          console.log(
+            " Number of records inserted on pedidos: " +
+              result.affectedRows
+          );
+          res.end(JSON.stringify(result));
+        });
+      }
     }
-  });
+  );
 });
 
 //rest api to get a single pedido data
@@ -194,55 +231,61 @@ app.get("/productos", function (req, res) {
 app.post("/pedido", function (req, res) {
   var params = req.body;
   console.log(params);
-  connection.query("INSERT INTO pedido SET ?", params, function (
-    error,
-    results,
-    fields
-  ) {
-    if (error) throw error;
-    res.end(JSON.stringify(results));
-  });
-});
-//multiple inserts on pedidosProductos Items 
-app.post("/pedido/items", function (req, res) {
-  connection.query("select max(idpedido) as masa from pedido;", function (
-    err,
-    result,
-    fields
-  ) {
-    if (err) throw err;
-    console.log(result[0].masa,"The last insert id in pedido");
-    console.log(req.body);
-    if (result.length > 0) {
-      var sql =
-        "INSERT INTO pedidoProductos (pedido_idpedido, producto_idproducto, cantidadComp) VALUES ?";
-      var values = [];
-      for (var i = 0; i < req.body.length; i++) {
-        values.push([result[0].masa, req.body[i].producto_idproducto, req.body[i].cantidadComp]);
-      }
-      connection.query(sql, [values], function (err, result) {
-        if (err) throw err;
-        console.log(" Number of records inserted on pedidosProductos: " + result.affectedRows);
-        res.end(JSON.stringify(result));
-      });
-    } else {
-      console.log("No data found");
+  connection.query(
+    "INSERT INTO pedido SET ?",
+    params,
+    function (error, results, fields) {
+      if (error) throw error;
+      res.end(JSON.stringify(results));
     }
-  });
+  );
+});
+//multiple inserts on pedidosProductos Items
+app.post("/pedido/items", function (req, res) {
+  connection.query(
+    "select max(idpedido) as masa from pedido;",
+    function (err, result, fields) {
+      if (err) throw err;
+      console.log(result[0].masa, "The last insert id in pedido");
+      console.log(req.body);
+      if (result.length > 0) {
+        var sql =
+          "INSERT INTO pedidoProductos (pedido_idpedido, producto_idproducto, cantidadComp) VALUES ?";
+        var values = [];
+        for (var i = 0; i < req.body.length; i++) {
+          values.push([
+            result[0].masa,
+            req.body[i].producto_idproducto,
+            req.body[i].cantidadComp,
+          ]);
+        }
+        connection.query(sql, [values], function (err, result) {
+          if (err) throw err;
+          console.log(
+            " Number of records inserted on pedidosProductos: " +
+              result.affectedRows
+          );
+          res.end(JSON.stringify(result));
+        });
+      } else {
+        console.log("No data found");
+      }
+    }
+  );
 });
 
 //rest api to create a enbed sql inserts
 app.post("/pedido", function (req, res) {
   var params = req.body;
   console.log(params);
-  connection.query("INSERT INTO pedido SET ?", params, function (
-    error,
-    results,
-    fields
-  ) {
-    if (error) throw error;
-    res.end(JSON.stringify(results));
-  });
+  connection.query(
+    "INSERT INTO pedido SET ?",
+    params,
+    function (error, results, fields) {
+      if (error) throw error;
+      res.end(JSON.stringify(results));
+    }
+  );
 });
 
 //rest api to update record into mysql database
